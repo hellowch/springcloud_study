57bbb5b3953f97d683acdf7107ec52c3f3f950f4
魏陈昊 <525691465@qq.com>
2020-11-19 17:22:18 +0800

    添加了hystrix流监控功能,创建dashboard项目，在8001的启动项中加如注解和@Bean,8001中需要加监控的pom

--- springcloud-provider-dept-8001/src/main/java/com/cloud/springcloud/DeptProvider_8001.java
+++ springcloud-provider-dept-8001/src/main/java/com/cloud/springcloud/DeptProvider_8001.java
@@ -1,10 +1,14 @@
 package com.cloud.springcloud;
 
 
+import com.netflix.hystrix.HystrixMetrics;
+import com.netflix.hystrix.contrib.metrics.eventstream.HystrixMetricsStreamServlet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableEurekaClient  //在启动后自动注册到eureka中
@@ -13,4 +17,6 @@ public class DeptProvider_8001 {
     public static void main(String[] args){
         SpringApplication.run(DeptProvider_8001.class,args);
     }
+
+
 }
--- springcloud-provider-dept-hystrix-8001/src/main/java/com/cloud/springcloud/DeptProviderHystrix_8001.java
+++ springcloud-provider-dept-hystrix-8001/src/main/java/com/cloud/springcloud/DeptProviderHystrix_8001.java
@@ -1,11 +1,14 @@
 package com.cloud.springcloud;
 
 
+import com.netflix.hystrix.contrib.metrics.eventstream.HystrixMetricsStreamServlet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableEurekaClient  //在启动后自动注册到eureka中
@@ -15,4 +18,13 @@ public class DeptProviderHystrix_8001 {
     public static void main(String[] args){
         SpringApplication.run(DeptProviderHystrix_8001.class,args);
     }
+
+    //增加一个servlet 用于被监控
+    @Bean
+    public ServletRegistrationBean hystrixMetricsStreamServlet(){
+        ServletRegistrationBean registrationBean = new ServletRegistrationBean(new HystrixMetricsStreamServlet());
+        registrationBean.addUrlMappings("/actuator/hystrix.stream");
+        return registrationBean;
+    }
+
 }
