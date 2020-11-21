8837aaed5bb1f9d13049c8656dd497cf5d2a1db3
魏陈昊 <525691465@qq.com>
2020-11-21 16:39:04 +0800

    设置config客户端与服务端

---
+++ springcloud-config-client-3355/src/main/java/com/cloud/springcloud/ConfigClient_3355.java
@@ -0,0 +1,12 @@
+package com.cloud.springcloud;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigClient_3355 {
+    public static void main(String[] args){
+        SpringApplication.run(ConfigClient_3355.class,args);
+    }
+}
